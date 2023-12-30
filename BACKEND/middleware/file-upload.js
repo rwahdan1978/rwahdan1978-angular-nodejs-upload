4317150ddd25081b19a30a25f3b25974e3403cc3
rwahdan1978 <ramiwahdan1978@gmail.com>
2023-12-30 09:48:54 +0400

    init1
    init1

---
+++ BACKEND/middleware/file-upload.js
@@ -0,0 +1,27 @@
+   const aws = require('aws-sdk');
+   const multer = require('multer');
+   const multerS3 = require('multer-s3');
+   const dotenv = require('dotenv');
+   dotenv.config();
+    
+   aws.config.update({
+    secretAccessKey: "oPjF19mEBbsa/7cPSHGELR9BBa0UswSfkXy5hvGe",
+    accessKeyId: "AKIAT4QNA4ENHNUEOL7T",
+    region: 'ap-south-1'
+   });
+   
+   const s3 = new aws.S3();
+   const upload = multer({
+   storage: multerS3({
+    acl: 'public-read',
+    s3,
+    bucket: 'angular-upload-files-2023-2024',
+    key: function(req, file, cb) {
+      req.file = file.originalname;
+      //include the folder created before uploading files...
+      cb(null, "properties2023/" + file.originalname);
+     }
+    })
+   });
+
+   module.exports = upload;
---
+++ BACKEND/server.js
@@ -0,0 +1,30 @@
+const upload = require('./middleware/file-upload');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const dotenv = require('dotenv');
+dotenv.config();
+/* Import your upload middleware */
+
+app.use(bodyParser.json());
+app.use(cors());
+
+/* You POST route here */
+
+/* Where image is the name of the property sent from angular via the Form Data and the 1 is the max number of files to upload*/
+app.post('/api/v1/upload/', upload.array('image', 1), (req, res) => {
+  /* This will be the response sent from the backend to the frontend */
+
+  res.send({ image: req.file });
+  //res.send({ name: req.body });
+
+ });
+
+const PORT = process.env.PORT || 3000;
+app.listen(3000, () => {
+  console.log(`server started on port 3000`);
+});
